5886445930ba6d0a0ee6ccb38468dce445458590
Bohdan Dovhal <46327995+H0us5@users.noreply.github.com>
2023-07-29 20:02:30 +0300

    setup fetching single product info and start to setup single product page

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,17 +15,74 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const SingleProduct = () => {
+  const { id } = useParams();
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext();
   const navigate = useNavigate();
-  return <h4>single product page</h4>;
-};
 
-// useEffect(()=>{
-// 	if (error) {
-// 		setTimeout(() => {
-// 			navigate('/')
-// 		},3000)
-// 	}
-// }, [error])
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  }, [error]);
+  if (loading) {
+    return <Loading />;
+  }
+  const {
+    name,
+    price,
+    description,
+    stock,
+    starts,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product;
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
+          back to products
+        </Link>
+        <div className="product-center">
+          <ProductImages />
+          <section className="content">
+            <h2>{name}</h2>
+            <Stars />
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="description">{description}</p>
+            <p className="info">
+              <span>Available : </span>
+              {stock > 0 ? "In stock" : "out of stock"}
+            </p>
+            <p className="info">
+              <span>SKU : </span>
+              {sku}
+            </p>
+            <p className="info">
+              <span>Brand : </span>
+              {company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.main`
   .product-center {
